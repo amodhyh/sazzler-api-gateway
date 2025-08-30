a8fa7bc2590ee3e346965fb4351f3f83912cd706
Amodhyh <35001977+amodhyh@users.noreply.github.com>
2025-08-30 23:45:54 +0530

    initial commit

---
+++ src/main/java/com/sazzler/ecommerce/sazzlerapigateway/SazzlerApiGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.sazzler.ecommerce.sazzlerapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SazzlerApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SazzlerApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sazzler/ecommerce/sazzlerapigateway/SazzlerApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sazzler.ecommerce.sazzlerapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SazzlerApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
